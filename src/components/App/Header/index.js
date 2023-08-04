07215be2493d43f20c9fc74ae5c748df75b8dc74
Miroslav <savovsky@gmail.com>
2023-08-04 13:13:07 +0300

    Header Component - added

---
+++ src/components/App/Header/index.js
@@ -0,0 +1,14 @@
+// @flow
+
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="app-header" data-testid="app-header">
+            <h1>DHL FRT ESS</h1>
+            <h2>React JS Dev - Interview Task</h2>
+        </header>
+    );
+}
+
+export default Header;
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 import LoaderSpin from '../LoaderSpin';
 import ComboBox from '../ComboBox';
+import Header from './Header';
 
 import { useFetchAlbumsQuery } from '../../store/apiAlbums';
 
@@ -20,7 +21,7 @@ function App() {
 
     return (
         <main className="app-container" data-testid="app-container">
-            <h1>DHL FRT ESS - React JS Dev - Interview Task</h1>
+            <Header />
             <LoaderSpin />
             <ComboBox
                 inputId="albums"
